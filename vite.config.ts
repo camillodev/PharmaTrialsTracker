82a4cd6b25e9fdf53cc25075d808f41529fa9158
Rafael Camillo <camillodeveloper@gmail.com>
2025-02-02 13:22:12 -0300

    fix layout

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,23 @@
-
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path, { dirname } from "path";
-import { fileURLToPath } from "url";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import themePlugin from '@replit/vite-plugin-shadcn-theme-json';
+import path, { dirname } from 'path';
+import runtimeErrorOverlay from '@replit/vite-plugin-runtime-error-modal';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-
 export default defineConfig({
-  plugins: [
-    react(),
-    // You can add other Vite plugins here if needed
-  ],
+  plugins: [react(), runtimeErrorOverlay(), themePlugin()],
   resolve: {
     alias: {
-      "@db": path.resolve(__dirname, "db"),
-      "@": path.resolve(__dirname, "client", "src"),
+      '@db': path.resolve(__dirname, 'db'),
+      '@': path.resolve(__dirname, 'client', 'src'),
     },
   },
-  root: path.resolve(__dirname, "client"),
+  root: path.resolve(__dirname, 'client'),
   build: {
-    outDir: path.resolve(__dirname, "dist/public"),
+    outDir: path.resolve(__dirname, 'dist/public'),
     emptyOutDir: true,
   },
 });
